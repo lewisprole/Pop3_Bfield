8ecf001584841b2c4621d54c014865ef42302bf3
lewis prole <prolel@cardiff.ac.uk>
2019-10-18 13:39:49 +0100

    m

---
+++ lewis_arepo_code/turbulance.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 12:14:11 2019
+
+@author: lewisprole
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+
+'''script to add turbulence'''
