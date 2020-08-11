318adc4dc35b2076dd9e7ebae5fde818c888e911
Lewis Prole <dc-prol1@login5a.pri.cosma7.alces.network>
2020-08-11 19:18:43 +0100

    .

--- lewis_arepo_code/hdf5_read.py
+++ lewis_arepo_code/hdf5_read.py
@@ -1,21 +1,25 @@
-import nump as np
+import numpy as np
 import h5py 
 
-def read(filename)
-	hf = h5py.File(filename, 'r')
-	keys=list(hf.keys())
-	particle_keys=list(hf[keys[-2]])
-	sink_keys=list(hf[keys[-1]])
+def read(filename):
+	file = h5py.File('snap_000.hdf5','r')
+	keys=list(file.keys())
+	particle_keys=list(file[keys[3]])
+	if 'PartType5' in keys:
+		sink_keys=list(file[keys[4]])
 
 	class a():
 		pass
-	
+	header=np.array(list(file.get("Header").attrs.values()))
+	a.boxsize=header[0]
+	a.npart=header[14]
+	a.time=header[20]
 	for key in particle_keys:
 		data=np.array(hf[keys[-2]][key])
 		if key=='Coordinates':
 			a.x,a.y,a.z=data[:,0],data[:,1],data[:,2]
 		if key=='Velocities':
-			a.vx,a.vy,a.vz=data[:,0],data[;,1],data[:,2]
+			a.vx,a.vy,a.vz=data[:,0],data[:,1],data[:,2]
 
 			
 			
---
+++ lewis_arepo_code/radial_profiles.py
@@ -0,0 +1,148 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import code_units
+import sys
+import io
+import arepo_utils
+import astropy.constants as ap
+
+
+def Rs(a):
+	mid=np.where(a.rho==a.rho.max())
+	rs=np.sqrt((a.x-a.x[mid])**2+(a.y-a.y[mid])**2+(a.z-a.z[mid])**2)
+	return rs
+
+
+def solenoidal_velocity(a):
+        '''calculates radial and non radial component of the velocity, also returns total vel magnitude'''
+        #create vector to center
+        mid=np.where(a.rho==a.rho.max())
+        xmid=a.x[mid]
+        ymid=a.y[mid]
+        zmid=a.z[mid]
+        r=np.array([a.x-xmid, a.y-ymid, a.z-zmid])
+        #radial and non-radial velocities
+        dot_product=(r[0]*a.vx + r[1]*a.vy + r[2]*a.vz)
+        cross_product=(   (r[1]*a.vz - r[2]*a.vy) , - (r[0]*a.vz - r[2]*a.vx) , (r[0]*a.vy - r[1]*a.vx)   )
+
+        #normalise
+        R=np.sqrt(r[0]**2+r[1]**2+r[2]**2)
+        radial_v=abs(dot_product/R)
+        nonrad_v=abs(cross_product/R)
+        #remove NaNs
+        radial_v[np.where(R==0)]=0
+        nonrad_v[:,np.where(R==0)]=np.zeros((3,1,1))
+        #reduce cross product to 1D
+        nonrad_v=np.sqrt(nonrad_v[0]**2 + nonrad_v[1]**2 + nonrad_v[2]**2)
+
+        v_mag=np.sqrt(a.vx**2+a.vy**2+a.vz**2)
+        return radial_v,nonrad_v,v_mag,R
+
+def abundances(a):
+	H2=a.chem[:,0]
+	e=a.chem[:,1]+a.chem[:,2]+a.chem[:,4]+2*a.chem[:,5]
+	return H2,e
+	
+
+def weighter(a,type):
+
+	if type=='volume':
+		normaliser=a.mass/a.rho
+	if type=='mass':
+		normaliser=a.mass
+	return normaliser
+
+
+def radial_average(variable,a,weight_type,bins):
+	rs=Rs(a)
+	x=np.linspace(np.log10(np.sort(rs)[1]),np.log10(rs.max()),bins)
+	x=10**x
+	weight=weighter(a,weight_type)
+	avs=np.array([])
+	for i in range(bins-1):
+		mask=np.where((rs>x[i]) & (rs<x[i+1]))
+		RS=rs[mask]
+		if sum(weight[mask])>0:
+			av=sum(variable[mask]*weight[mask])/sum(weight[mask])
+		else:
+			av=0
+		avs=np.append(avs,av)
+	return x,avs
+	
+def cycle_plot(snaps,weight_type,bins,labels):
+	
+	fig,axs=plt.subplots(6,sharex=True)
+	plt.subplots_adjust(wspace=0, hspace=0,top=0.9,bottom=0.1)
+	for i in range(1):#len(snaps)):
+		a=arepo_utils.aread(snaps)#[i])
+		rv,nv,v,rs=solenoidal_velocity(a)
+		H2,e=abundances(a)
+
+		print('averaging')
+		x,v=radial_average(v,a,weight_type,bins)
+		print('v')
+		x,rho=radial_average(a.rho,a,weight_type,bins)
+		print('rho')
+		x,rv=radial_average(rv,a,weight_type,bins)
+		print('radial v')
+		x,nv=radial_average(nv,a,weight_type,bins)
+		print('rotational v')
+		x,T=radial_average(a.temp,a,weight_type,bins)
+		print('T')
+		x,H2=radial_average(H2,a,weight_type,bins)
+		print('H2')
+		x,e=radial_average(e,a,weight_type,bins)
+		print('e')
+		
+
+		rho=rho*code_units.rho_cu
+		v=v*code_units.v_cu/1e5
+		rv=rv*code_units.v_cu/1e5
+		nv-nv*code_units.v_cu/1e5
+		x=x*code_units.d_cu/ap.pc.cgs.value
+		
+		x=x[:-1]
+		axs[0].loglog(x,rho,label=labels[i])
+		axs[1].semilogx(x,rv)
+		axs[2].semilogx(x,nv)
+		axs[3].loglog(x,T)
+		axs[4].loglog(x,H2)
+		axs[5].loglog(x,e)
+	
+	axs[5].set_xlabel('R [pc]')
+	
+	axs[0].set_ylabel(r'$\rho$ [gcm$^{-3}$]')
+	logrange=np.log10(rho.max())-np.log10(rho.min())
+	p=np.round( np.log10(rho.min()) + logrange*np.array([0.25,0.5,0.75]) ,0)
+	axs[0].set_yticks(10**(p.astype(float)))
+
+	axs[1].set_ylabel(r'$v_{r}$ [kms$^{-1}$]')
+	
+	axs[1].set_yticks((rv.min() + (rv.max()-rv.min()) * np.array([0.25,0.5,0.75])).astype(int))
+
+	axs[2].set_ylabel(r'$v_{\theta}$ [kms$^{-1}$]')
+	t=axs[2].get_yticks()
+	axs[2].set_yticks((nv.min() + (nv.max()-nv.min()) * np.array([0.25,0.5,0.75])).astype(int))
+
+	axs[3].set_ylabel('T [K]')
+	logrange=np.log10(T.max())-np.log10(T.min())
+	p=np.round( np.log10(T.min()) + logrange*np.array([0.25,0.5,0.75]) ,0)
+	axs[3].set_yticks(10**(p.astype(float)))
+	
+	axs[4].set_ylabel('H2')
+	logrange=np.log10(H2.max())-np.log10(H2.min())
+	p=np.round( np.log10(H2.min()) + logrange*np.array([0.25,0.5,0.75]) ,0)
+	axs[4].set_yticks(10**(p.astype(float)))
+
+	axs[5].set_ylabel(r'$e^{-}$')
+	logrange=np.log10(e.max())-np.log10(e.min())
+	p=np.round( np.log10(e.min()) + logrange*np.array([0.25,0.5,0.75]) ,0)
+	axs[5].set_yticks(10**(p.astype(float)))
+
+
+
+
+
+
+
+
