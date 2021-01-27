725a58980f11aaab40ff49ca6df434f302797318
Lewis Prole <prolel@cardiff.ac.uk>
2021-01-27 12:19:19 +0000

    .

--- lewis_arepo_code/cooling_plot.py
+++ lewis_arepo_code/cooling_plot.py
@@ -9,51 +9,8 @@ from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt 
 
 
-def plot(file):
-	'''2d histograms of heating and cooling rate vs rho, with free-fall time plotted'''
-	a=arepo_utils.aread(file)
-	
-	#radial distances fro CoM 
-	mid=np.where(a.rho==a.rho.max())
-	r=np.sqrt((a.x-a.x[mid])**2+(a.y-a.y[mid])**2+(a.z-a.z[mid])**2)
 
-	#create args that separate heating and cooling terms in a.cooling
-	heat=np.array([8,9,10,11,12,20,21,22,23,24,25])
-	cool=np.array([0,1,2,3,4,6,7,13,14,15,16,17,18,19,26])
-
-	#calculate free-fall times 
-	tff=np.sqrt(3*np.pi/(32*ap.G.cgs.value*a.rho*code_units.rho_cu))
-
-	#2d histogram of heating vs rho
-	x,y,z=np.histogram2d( np.log10(abs(a.u/np.sum(a.cooling[:,heat],1)) *code_units.t_cu/(60*60*24*365)),np.log10(a.rho*code_units.rho_cu),bins=(800,800))
-	line1=plt.imshow(x/x,cmap='autumn',aspect='auto',label=r'$t_h$',extent=[z[0],z[-1],y[-1],y[0]])
-	#2d histogram of cooling vs rho
-	x,y,z=np.histogram2d(np.log10(abs(a.u/np.sum(a.cooling[:,cool],1))*code_units.t_cu/(60*60*24*365)),np.log10(a.rho*code_units.rho_cu),bins=(800,800))
-	line2=plt.imshow(x/x,cmap='winter',aspect='auto',label=r'$t_c$',extent=[z[0],z[-1],y[-1],y[0]])
-	plt.ylim(y[0],y[-1])
-
-	#plot free-fall time 
-	rho=10**np.linspace(z[0],z[-1],100)
-	tff=np.sqrt(3*np.pi/(32*ap.G.cgs.value*rho))/(60*60*24*365)
-	line3=plt.plot(np.log10(rho),np.log10(tff),'k',label=r'$t_{ff}$')
-
-	#create legend patches
-	line1=Line2D([0], [0], color='r', lw=2)
-	line2=Line2D([0], [0], color='b', lw=2)
-	line3=Line2D([0], [0], color='k', lw=2)
-	plt.legend([line1,line2,line3],(r'$t_h$',r'$t_c$',r'$t_{ff}$'),fontsize=12,frameon=False,markerscale=10)
-
-	#configure subplots
-	plt.subplots_adjust(left = 0.2,bottom = 0.17,right=0.9)
-	plt.tick_params(axis="x", labelsize=15,direction="in")
-	plt.tick_params(axis="y", labelsize=15,direction="in")
-	plt.xlabel(r'log$_{10}(\rho$ [gcm$^{-3}$])',fontsize=20)
-	plt.ylabel(r'log$_{10}(t$ [yrs])       ',fontsize=20,rotation=90)
-	plt.ylim(np.log10(tff).min()-1,y[-1])
-
-
-
-'''||||||| alternate method using average mass to calculate free-fall time: gives the same result as above - no point |||||||'''
+'''||||||| average mass to calculate free-fall time  |||||||'''
 
 
 def Tff(a):
@@ -75,35 +32,90 @@ def function(tff,rs):
 	f=interp1d(rs,tff,fill_value="extrapolate")
 	return f
 
-def plot_alt(file):
-	'''2d histograms of heating and cooling vs radial distance, with t_ff plotted'''
-	a=arepo_utils.aread(file)
-	mid=np.where(a.rho==a.rho.max())
-	r=np.sqrt((a.x-a.x[mid])**2+(a.y-a.y[mid])**2+(a.z-a.z[mid])**2)
+
+
+
+
+
+
+
+
+''' ||||||||||| compressional heating rate per mass |||||||||||'''
+def compression(a):
+	return ap.k_B.cgs.value * a.temp / ap.m_p.cgs.value * np.sqrt(32*ap.G.cgs.value/(2*np.pi)) * np.sqrt(a.rho*code_units.rho_cu)
+
+
+'''||||||||| working out the accretion heating rate per mass |||||||||||'''
+
+def accretion(file1,file2,Rstar):
+	a=arepo_utils.aread(file2)
+	b=arepo_utils.aread(file1)
+	dt=(a.time-b.time )*code_units.t_cu
+	heating = np.zeros_like(a.rho)
+	for i in range(len(a.sinkmass)):
+		dM=(a.sinkmass[i]-b.sinkmass[i])*code_units.M_cu
+		rate=dM/dt
+		L=ap.G.cgs.value*a.sinkmass[i]*code_units.M_cu * rate / Rstar
+		r=np.sqrt((a.x-a.sinkx[i])**2+(a.y-a.sinky[i])**2+(a.z-a.sinkz[i])**2) *code_units.d_cu
+		heating +=  a.rho*code_units.rho_cu * L /(4*np.pi * r**2)   #per mass 
+	return heating 
+		
+
+''' |||||||| e = rho * u, de/dt = drho/dt + du/dt, we want du/dt (rate per mass)'''
+'''---> du/dt = de/dt - drho/dt, use continuity equation for drho/dt |||||||'''
+	
+def du_dt(a):
+	drho_dt =  -a.divv * a.rho*code_units.rho_cu
+	
+	#create args that separate heating and cooling terms in a.cooling
 	heat=np.array([8,9,10,11,12,20,21,22,23,24,25])
 	cool=np.array([0,1,2,3,4,6,7,13,14,15,16,17,18,19,26])
-	tff,rs=Tff(a)
-	#T=function(tff,rs)
-	#tff=T(r*code_units.d_cu)
-	mask=np.where(r>0)
-	x,y,z=np.histogram2d(np.log10(abs(a.u*a.rho/np.sum(a.cooling[:,heat],1))[mask]*code_units.t_cu/(60*60*24*365)),np.log10(r[mask]*code_units.d_cu),bins=(800,800))
-	line1=plt.imshow(x/x,cmap='autumn',aspect='auto',label=r'$t_h$',extent=[z[0],z[-1],y[-1],y[0]])
-	x,y,z=np.histogram2d(np.log10(abs(a.u*a.rho/np.sum(a.cooling[:,cool],1))[mask]*code_units.t_cu/(60*60*24*365)),np.log10(r[mask]*code_units.d_cu),bins=(800,800))
-	line2=plt.imshow(x/x,cmap='winter',aspect='auto',label=r'$t_c$',extent=[z[0],z[-1],y[-1],y[0]])
+	
+	#convert cooling rate per volume into cooling rate per mass 
+	for i in range(a.cooling.shape[1]):
+		a.cooling[:,i] = a.cooling[:,i] - drho_dt
+
+	#total heating and  cooling rate per mass
+	heat_rate= np.sum(a.cooling[:,heat],1) 
+	cool_rate= np.sum(a.cooling[:,cool],1) 
+	
+	return heat_rate, cool_rate 
+
+'''|||||||plot heating and cooling vs density|||||||'''
+
+def plot(file1,file2,Rstar):
+	a=arepo_utils.aread(file2)
+	heating,cooling =du_dt(a)
+	comp=compression(a)
+	acc=accretion(file1,file2,Rstar)
+
+	HEATING,y,z=np.histogram2d( np.log10(-heating)[~np.isnan(np.log10(-heating))],       np.log10(a.rho*code_units.rho_cu)[~np.isnan(np.log10(-heating))],bins=(800,800))
+	plt.imshow(HEATING/HEATING,cmap='RdYlBu',aspect='auto',label=r'$-\Gamma$',extent=[z[0],z[-1],y[-1],y[0]])
+
+	ACC,y,z=np.histogram2d( np.log10(acc),np.log10(a.rho*code_units.rho_cu),bins=(800,800))
+	plt.imshow(ACC/ACC,cmap='spring',aspect='auto',label=r'$Gamma_L$',extent=[z[0],z[-1],y[-1],y[0]])
+
+	COMP,y,z=np.histogram2d( np.log10(comp),np.log10(a.rho*code_units.rho_cu),bins=(800,800))
+	plt.imshow(COMP/COMP,cmap='PRGn',aspect='auto',label=r'$k_B T/m_p \sqrt{\frac{32G\rho}{3\pi}$',extent=[z[0],z[-1],y[-1],y[0]])
+
+	COOLING,y,z=np.histogram2d( np.log10(cooling)[~np.isnan(np.log10(cooling))],  np.log10(a.rho*code_units.rho_cu)[~np.isnan(np.log10(cooling))],  bins=(800,800))
+	plt.imshow(COOLING/COOLING,cmap='winter',aspect='auto',label=r'$\Lambda$',extent=[z[0],z[-1],y[-1],y[0]])
+
+	#TOTHEAT,y,z=np.histogram2d( np.log10(-heating+comp+acc),np.log10(a.rho*code_units.rho_cu),bins=(800,800))
+	#plt.imshow(TOTHEAT/TOTHEAT,cmap='afmhot',aspect='auto',label=r'$\Lambda$',extent=[z[0],z[-1],y[-1],y[0]])
+
 	plt.ylim(y[0],y[-1])
-	#rho=10**np.linspace(z[0],z[-1],100)
-	#tff=np.sqrt(3*np.pi/(32*ap.G.cgs.value*rho))/(60*60*24*365)
-	line3=plt.plot(np.log10(rs),np.log10(tff),'k',label=r'$t_{ff}$')
-	line1=Line2D([0], [0], color='r', lw=2)
-	line2=Line2D([0], [0], color='b', lw=2)
-	line3=Line2D([0], [0], color='k', lw=2)
-	plt.legend([line1,line2,line3],(r'$t_h$',r'$t_c$',r'$t_{ff}$'),fontsize=12,frameon=False,markerscale=10,loc='upper left')
-	plt.subplots_adjust(left = 0.2,bottom = 0.17,right=0.9)
-	plt.tick_params(axis="x", labelsize=15,direction="in")
-	plt.tick_params(axis="y", labelsize=15,direction="in")
-	plt.xlabel(r'log$_{10}$(R [cm])',fontsize=20)
-	plt.ylabel(r'log$_{10}(t$ [yrs])       ',fontsize=20,rotation=90)
-	#plt.ylim(np.log10(tff).min()-1,y[-1])
+
+	plt.ylabel(r'erg s$^{-1}$ g $^{-1}$')
+	plt.xlabel(r'$\rho$ [g cm$^{-3}$]')
+
+	line1=Line2D([0], [0], color='brown', lw=2)
+	line2=Line2D([0], [0], color='indigo', lw=2)
+	line3=Line2D([0], [0], color='fuchsia', lw=2)
+	line4=Line2D([0], [0], color='b', lw=2)
+	plt.legend([line1,line2,line3,line4],(r'$\Gamma$',r'$\Gamma_L$',r'$\frac{k_B T}{m_p} \sqrt{\frac{32G\rho}{3\pi}}$',r'$-\Lambda$'),fontsize=10,frameon=False,markerscale=10,loc='upper left')
+	
+'''||| plot heating and cooling as functions of density and radial distance |||'''
 
 
 def both(file):
